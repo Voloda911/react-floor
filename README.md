b48a44f3f3a30b9b607776b8410eef4d95ac823a
Voloda911 <surikusvovcik@gmail.com>
2024-03-09 19:54:08 -0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# Test CRA Project
---
+++ precache-manifest.c4b900461477acef874ce49846e12040.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f1dbdc03a4e6ccbb05cb7d0bcdae5a",
+    "url": "/react-floor/index.html"
+  },
+  {
+    "revision": "01f37c05bf6708ffc282",
+    "url": "/react-floor/static/css/main.29f091d9.chunk.css"
+  },
+  {
+    "revision": "f7984da93a4adab6e27c",
+    "url": "/react-floor/static/js/2.5df2721e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-floor/static/js/2.5df2721e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f37c05bf6708ffc282",
+    "url": "/react-floor/static/js/main.11ae23b1.chunk.js"
+  },
+  {
+    "revision": "f3fd698d75b495e4d734",
+    "url": "/react-floor/static/js/runtime-main.8f77bd19.js"
+  },
+  {
+    "revision": "4ae069c893a721d6c0516653db3f5444",
+    "url": "/react-floor/static/media/LOGO1.0f51300e.png"
+  },
+  {
+    "revision": "5473d303f62ebb2fc48d8dbcf992d500",
+    "url": "/react-floor/static/media/forHowWorks.f5cecb6e.jpg"
+  },
+  {
+    "revision": "92367b25017a32b9129f9450ef98adbb",
+    "url": "/react-floor/static/media/forTest.8b6e51b2.jpg"
+  },
+  {
+    "revision": "245261d99607dea74978de02ba6fab94",
+    "url": "/react-floor/static/media/howWokrs.c5ddbfc2.jpg"
+  },
+  {
+    "revision": "857acf022ef66f15abd3fe122860c161",
+    "url": "/react-floor/static/media/instoll.9410d2da.jpg"
+  },
+  {
+    "revision": "e849d7775adba5bdc557dba2bd19fcdd",
+    "url": "/react-floor/static/media/pexels-cocarinne-7260267.33b2fec4.jpg"
+  },
+  {
+    "revision": "e7cea48c49bcb67ec18f478287b2f32d",
+    "url": "/react-floor/static/media/pexels-cottonbro-studio-7492882.53fa37c3.jpg"
+  },
+  {
+    "revision": "b038ae036da26afa94afeee8fdae5f12",
+    "url": "/react-floor/static/media/pexels-digital-buggu-218564.e9dff125.jpg"
+  },
+  {
+    "revision": "4fec42bd8431da9416cbcd2d215dd289",
+    "url": "/react-floor/static/media/pexels-jill-burrow-6681856.e78f4d80.jpg"
+  },
+  {
+    "revision": "339e9a716948a8800d93da86358996f9",
+    "url": "/react-floor/static/media/pexels-max-rahubovskiy-6447388.c0aba344.jpg"
+  },
+  {
+    "revision": "e4c35355d4cdbc248b1e7dde7c44a445",
+    "url": "/react-floor/static/media/pexels-max-rahubovskiy-6447390.8e1aadd4.jpg"
+  },
+  {
+    "revision": "160e1b9ecb78d0921bcc03d07715a25e",
+    "url": "/react-floor/static/media/pexels-max-rahubovskiy-6492397.9d329de9.jpg"
+  },
+  {
+    "revision": "23080604825ac114be40faf8e72aafd8",
+    "url": "/react-floor/static/media/pexels-max-rahubovskiy-6585761 (1).dc131b5e.jpg"
+  },
+  {
+    "revision": "8a804358478213d86284608daea64d1b",
+    "url": "/react-floor/static/media/pexels-max-rahubovskiy-6636293.bd5f2352.jpg"
+  },
+  {
+    "revision": "9a0ac813b41178dfc77e8742b2b7fe30",
+    "url": "/react-floor/static/media/pexels-max-rahubovskiy-7018824.9849e95a.jpg"
+  },
+  {
+    "revision": "acf4507750c67c8140bbc21d8c2d5857",
+    "url": "/react-floor/static/media/pexels-max-rahubovskiy-7031723.736c6db6.jpg"
+  },
+  {
+    "revision": "47d6cfc4c6e6ce134d9906096cd39044",
+    "url": "/react-floor/static/media/pexels-max-rahubovskiy-7045844.a414d684.jpg"
+  },
+  {
+    "revision": "053ac89d237694b15d4c8999e736836d",
+    "url": "/react-floor/static/media/pexels-max-rahubovskiy-7214467.83bb2dda.jpg"
+  },
+  {
+    "revision": "25e72370b18903d2ea3621849a815488",
+    "url": "/react-floor/static/media/pexels-rachel-claire-4846106.89b2bb8e.jpg"
+  },
+  {
+    "revision": "c708e3ae9c3ee7954af412acf4332046",
+    "url": "/react-floor/static/media/pexels-rachel-claire-4993081.f605434c.jpg"
+  },
+  {
+    "revision": "bef8b80987973a0d1071beb108729b25",
+    "url": "/react-floor/static/media/pexels-skylar-kang-6046819.59e70804.jpg"
+  }
+]);
